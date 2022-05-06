fb8204c0c5f3b348ddf9341b83340682c2654a1c
jalegomez96 <jalegomez96@gmail.com>
2022-05-05 19:34:55 -0500

    Activity 2-3 TypesOfProducts list and form

--- src/app/components/type-of-products/components/form-product-type/form-product-type.component.ts
+++ src/app/components/type-of-products/components/form-product-type/form-product-type.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { CategoryModel } from 'src/app/core/models/categories.model';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ModalService } from 'src/app/shared/services/modal-service/modal.service';
 
 @Component({
   selector: 'app-form-product-type',
   templateUrl: './form-product-type.component.html',
-  styleUrls: ['./form-product-type.component.css']
+  styleUrls: ['./form-product-type.component.css'],
 })
-export class FormProductTypeComponent {
+export class FormProductTypeComponent implements OnInit {
+  @Output() saveEmit = new EventEmitter<CategoryModel>();
 
   form: FormGroup;
 
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly modelService: ModalService
+  ) {}
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm(): void {
+    this.form = this.fb.group({
+      id: ['', [Validators.required, Validators.maxLength(10)]],
+      name: ['', [Validators.required, Validators.maxLength(50)]],
+    });
+  }
+
+  onClickSave(): void {
+    if (this.form.valid) {
+      this.saveEmit.emit(this.form.value);
+    } else {
+      this.modelService.showModal('Invalid form', 'Check the form', 'Error');
+    }
+  }
 }
--- src/app/shared/services/category-service/category.service.ts
+++ src/app/shared/services/category-service/category.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { CategoryModel } from 'src/app/core/models/categories.model';
 import { typesOfProducts } from '../../constants/types-of-products.constant';
 
-
 @Injectable()
 export class CategoryService {
+  constructor() {}
 
-  constructor() { }
-
-  getCategory(): Observable<CategoryModel[]> {
+  getAllCategory(): Observable<CategoryModel[]> {
     return of(typesOfProducts);
   }
 
+  saveCategory(category: CategoryModel): Observable<CategoryModel> {
+    return of(category).pipe(
+      tap(() => {
+        typesOfProducts.push(category);
+      })
+    );
+  }
 }
